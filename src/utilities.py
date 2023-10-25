1d5358ec75bd10e3fde3c970ed2bd466458799fa
python <python@ubuntu22.myguest.virtualbox.org>
2023-10-25 20:22:55 +0600

    Refactor

--- src/utilities.py
+++ src/utilities.py
@@ -5,7 +5,7 @@ from urllib.parse import urlparse, parse_qs
 def get_param(url, param_key):
     query_string = parse_qs(urlparse(url).query)
 
-    return (query_string[param_key][0] if param_key in query_string else None)
+    return query_string[param_key][0] if param_key in query_string else None
 
 
 def parse_client_request(client_data):
